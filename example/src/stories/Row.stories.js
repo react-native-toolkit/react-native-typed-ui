5e5f498b4198c4baba2f009170ae95a9c1fc5709
DaniAkash <s.daniakash@gmail.com>
2020-09-01 06:13:37 +0530

    docs: created getting started page

---
+++ example/src/stories/Row.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Row, Box, TextBlock } from '../designSystem/theme';
+import ThemeDecorator from './decorators/ThemeDecorator';
+
+export default {
+  title: 'Layout Components/Row',
+  component: Row,
+  argTypes: {},
+  decorators: [ThemeDecorator],
+};
+
+const Template = (args) => <Row {...args} />;
+
+export const DefaultRow = Template.bind({});
+DefaultRow.args = {
+  height: 250,
+  width: 250,
+  backgroundColor: 'grey100',
+  children: <TextBlock>Hello...</TextBlock>,
+};
