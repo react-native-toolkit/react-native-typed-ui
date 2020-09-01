d496976083469bc4f435563e734217e032ec7d8f
DaniAkash <s.daniakash@gmail.com>
2020-09-01 06:09:11 +0530

    chore: update storybook config

--- example/.storybook/preview.js
+++ example/.storybook/preview.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { ThemeProvider } from '../src/designSystem/theme';
+import { themes } from '@storybook/theming';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  docs: {
+    theme: themes.dark,
+  },
 };
 
 export const decorators = [
