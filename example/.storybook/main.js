3c2ec67aba8203d272fbbb81eaaf37ffc134a196
DaniAkash <s.daniakash@gmail.com>
2020-09-17 16:09:30 +0530

    chore: use unimodules config in storybook

--- example/.storybook/main.js
+++ example/.storybook/main.js
@@ -1,10 +1,10 @@
+const { withUnimodules } = require('@expo/webpack-config/addons');
+const { resolve } = require('path');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  webpackFinal: async (config) => {
+    return withUnimodules(config, { projectRoot: resolve(__dirname, '../') });
+  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+};
--- example/.storybook/preview.js
+++ example/.storybook/preview.js
@@ -5,7 +5,7 @@ import { themes } from '@storybook/theming';
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   docs: {
-    theme: themes.dark,
+    theme: themes.light,
   },
 };
 
