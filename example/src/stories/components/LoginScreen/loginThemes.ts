fd671c16e6ceb508644e9563aeece54bf75b7539
DaniAkash <s.daniakash@gmail.com>
2020-09-17 20:21:24 +0530

    docs: created login themes

---
+++ example/src/stories/components/LoginScreen/loginThemes.ts
@@ -0,0 +1,109 @@
+const fontWeights: { normal: 'normal' } = { normal: 'normal' };
+
+export const loginThemeOne = {
+  colors: {
+    brand: '#5DB075',
+    text: '#666666',
+    placeholder: '#BDBDBD',
+    placeholderBackground: '#F6F6F6',
+    title: 'black',
+    background: 'white',
+  },
+  fonts: {
+    primary: 'roboto',
+  },
+  fontSizes: {
+    normalText: 16,
+    infoText: 14,
+    titleText: 30,
+  },
+  fontWeights,
+  lineHeights: {},
+  letterSpacings: {
+    title: 1.2,
+  },
+  spacing: {
+    normal: 16,
+  },
+  zIndices: {},
+  iconSizes: {
+    normal: 16,
+  },
+  borderRadius: {
+    inputField: 8,
+    button: 100,
+  },
+  shadows: {},
+};
+
+export const loginThemeTwo = {
+  colors: {
+    brand: '#5DB075',
+    text: '#666666',
+    placeholder: '#BDBDBD',
+    placeholderBackground: '#F6F6F6',
+    title: 'black',
+    background: 'white',
+  },
+  fonts: {
+    primary: 'roboto',
+  },
+  fontSizes: {
+    normalText: 16,
+    infoText: 14,
+    titleText: 30,
+  },
+  fontWeights,
+  lineHeights: {},
+  letterSpacings: {
+    title: 1.2,
+  },
+  spacing: {
+    normal: 16,
+  },
+  zIndices: {},
+  iconSizes: {
+    normal: 16,
+  },
+  borderRadius: {
+    inputField: 8,
+    button: 100,
+  },
+  shadows: {},
+};
+
+export const loginThemeThree = {
+  colors: {
+    brand: '#5DB075',
+    text: '#666666',
+    placeholder: '#BDBDBD',
+    placeholderBackground: '#F6F6F6',
+    title: 'black',
+    background: 'white',
+  },
+  fonts: {
+    primary: 'roboto',
+  },
+  fontSizes: {
+    normalText: 16,
+    infoText: 14,
+    titleText: 30,
+  },
+  fontWeights,
+  lineHeights: {},
+  letterSpacings: {
+    title: 1.2,
+  },
+  spacing: {
+    normal: 16,
+  },
+  zIndices: {},
+  iconSizes: {
+    normal: 16,
+  },
+  borderRadius: {
+    inputField: 8,
+    button: 100,
+  },
+  shadows: {},
+};
